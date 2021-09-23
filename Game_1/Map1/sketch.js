d0a09e751d2d3ebe5d6f23c8bdad7232d2967867
Layendan <layendan.h@gmail.com>
2021-09-23 10:39:22 -0700

    Added mapdata

--- Game_1/Map1/sketch.js
+++ Game_1/Map1/sketch.js
@@ -1,9 +1,20 @@
+var mapdata = fetch("mapdata.json")
+.then(response => {
+  return response.json();
+})
+.then(data => mapdata = data);
+
+console.log(mapdata);
+
 function setup() {
     createCanvas(1000,1000);
-  }
+}
   
-  function draw() {
+function draw() {
     background(255);
 
+    text(frameRate().toLocaleString(undefined,{ maximumFractionDigits: 0 })
+          ,10,30);
+
     circle(mouseX,mouseY,30);
-  }
+}
