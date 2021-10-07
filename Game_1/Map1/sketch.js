c46bcaf7e129d425ec2cb7142c34c1283a3b9a7c
Layendan <layendan.h@gmail.com>
2021-10-07 10:54:45 -0700

    added background image and fixed mapdata parsing

--- Game_1/Map1/sketch.js
+++ Game_1/Map1/sketch.js
@@ -1,27 +1,34 @@
-var mapdata = fetch("mapdata.json")
-.then(response => {
-  return response.json();
-})
-.then(data => mapdata = data);
-
+let mapdata, canvas, backgroundImage;
 const canvasDiv = document.getElementById('myCanvas');
 let parentWidth = canvasDiv.offsetWidth; // width of browser window
 let parentHeight = canvasDiv.offsetHeight; // height of browser window
-let canvas;
+
+fetch("mapdata.json")
+.then(response => {
+  return response.json();
+})
+.then(data => {
+  mapdata = data;
+})
 
 function setup() {
     canvas = createCanvas(parentWidth,parentHeight);
+    backgroundImage = loadImage('cover.jpg');
+    noCursor();
 
     Circle(0,0,0,0,0,0,0);
+
+    console.log(mapdata);
+
 }
   
 function draw() {
-    background(255);
-
-    text(frameRate().toLocaleString(undefined,{ maximumFractionDigits: 0 })
-          ,10,30);
+    background(backgroundImage);
 
     circle(mouseX,mouseY,30);
+
+    text(frameRate().toLocaleString(undefined,{ maximumFractionDigits: 0 })
+          ,10,20);
 }
 
 function onResize() {
