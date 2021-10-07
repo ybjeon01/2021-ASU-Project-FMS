333d49e80f3b6bde6bea4031b067ad5ceb0cca45
Archit Singh <91148814+architsingh0@users.noreply.github.com>
2021-10-07 09:37:53 -0700

    Displaying the maze in the center of the screen

--- Game_4/sketch.js
+++ Game_4/sketch.js
@@ -1,7 +1,14 @@
-function setup() {
-  createCanvas(1920,1080);
+//Initializing browser window canvas
+var canvasDiv = document.getElementById('myCanvas');
+var parentWidth = canvasDiv.offsetWidth;
+var parentHeight = canvasDiv.offsetHeight;
+let img;
+
+function setup(){
+  createCanvas(parentWidth, parentHeight);
+  img = loadImage('assets/maze.png');
 }
 
-function draw() {
-  background(255);
+function draw(){
+  image(img, width/2 - 300, height/2 - 300);
 }
