e31d03e5aafa8e40a4abb6ca70ede2e2251f14aa
Layendan <layendan.h@gmail.com>
2021-10-18 11:38:32 -0700

    added circles and tested on first map

--- Game_1/Game_Components/circle.js
+++ Game_1/Game_Components/circle.js
@@ -1,12 +1,64 @@
-var circleSize, accuracy, approachRate, x, y, circleColor, number, hitSound;
+var circleSize, approachSize, accuracy, approachRate, x, y, circleColor, number, hitSound, failSound, time, isActive;
 
-function Circle(circleSize, accuracy, approachRate, x, y, color, number) {
-    this.size = circleSize;
+function Circle(circleSize, accuracy, approachRate, x, y, color, number, time) {
+    this.circleSize = circleSize * 20;
     this.accuracy = accuracy;
     this.approachRate = approachRate;
     this.x = x;
     this.y = y;
     this.circleColor = color;
     this.number = number;
+    this.time = time;
+    isActive = false;
     hitSound = loadSound('../Game_Components/assets/drum-hitnormal.wav');
+    failSound = loadSound('../Game_Components/assets/fail-sound.wav');
+
+    this.update = function (currentTime) {
+        let timeDiff = this.time - currentTime;
+        if (timeDiff < 0) {
+            fail();
+            return -1;
+        };
+        this.approachSize = this.circleSize + (timeDiff * this.approachRate);
+
+        addCircle(this.x, this.y, this.circleSize, this.approachSize);
+
+        isActive = true;
+
+        return 0;
+    };
+
+    this.click = function (mouseX, mouseY, songTime) {
+        let distance = Math.sqrt(Math.pow(mouseX - x, 2) + Math.pow(mouseY - y, 2));
+        console.log(distance);
+        console.log(distance <= this.circleSize);
+        if (distance <= circleSize) {
+            if (Math.abs(this.time - songTime) <= 0.5) {
+                success();
+                return 1;
+            } else {
+                fail();
+                console.log("Fail 1");
+                return -1;
+            }
+        } else if (distance <= this.approachSize) {
+            fail();
+            console.log("Fail 2");
+            return -1;
+        } else {
+            return 0;
+        }
+    };
+
+    return this;
+}
+
+function success() {
+    //hitSound.play();
+    console.log("success");
+}
+
+function fail() {
+    //failSound.play();
+    console.log("fail");
 }
--- Game_1/Map1/sketch.js
+++ Game_1/Map1/sketch.js
@@ -1,34 +1,97 @@
-let mapdata, canvas, backgroundImage;
+let mapdata, mapping, canvas, backgroundImage, isPlaying, song;
+let circles; // array of circles
+let active; // array of active circles
 const canvasDiv = document.getElementById('myCanvas');
 let parentWidth = canvasDiv.offsetWidth; // width of browser window
 let parentHeight = canvasDiv.offsetHeight; // height of browser window
 
+let temp;
+
 fetch("mapdata.json")
-.then(response => {
-  return response.json();
-})
-.then(data => {
-  mapdata = data;
-})
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    mapdata = data;
+  });
+
+fetch("mapping.json")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    mapping = data;
+  });
+
+function preload() {
+  song = loadSound('My_Love.mp3');
+
+  backgroundImage = loadImage('cover.jpg');
+  backgroundImage.filter(ERODE);
+  backgroundImage.filter(BLUR, 10);
+
+  circles = [];
+  active = [];
+}
 
 function setup() {
-    canvas = createCanvas(parentWidth,parentHeight);
-    backgroundImage = loadImage('cover.jpg');
-    noCursor();
+  canvas = createCanvas(parentWidth, parentHeight);
 
-    Circle(0,0,0,0,0,0,0);
+  isPlaying = true;
 
-    console.log(mapdata);
+  noCursor();
+  frameRate(120);
 
+  console.log(mapdata);
+  console.log(mapping);
+
+  temp = new Circle(4, 4, 4, 50, 50, 'red', 1, 5);
+
+  console.log(temp);
+
+  circles.push(temp);
+  circles.push(new Circle(4, 4, 4, 150, 250, 'red', 1, 7))
+
+  song.play();
 }
-  
+
 function draw() {
-    background(backgroundImage);
+  background(backgroundImage);
 
-    circle(mouseX,mouseY,30);
+  let currentTime = song.currentTime();
 
-    text(frameRate().toLocaleString(undefined,{ maximumFractionDigits: 0 })
-          ,10,20);
+  for (let i = 0; i < circles.length && i <= 5; i++) {
+    if (!circles[i].isActive && currentTime > (circles[i].time - 5)) {
+      active.push(circles[i]);
+      circles[i].isActive = true;
+      circles.splice(i, 1);
+    }
+  }
+
+  for (let i = active.length - 1; i >= 0; i--) {
+    let response = active[i].update(currentTime);
+    if (response === -1) {
+      active.splice(i, 1);
+    }
+  }
+
+  stroke(0);
+  fill(255);
+  circle(mouseX, mouseY, 30);
+
+  noStroke();
+  fill(0);
+  text(frameRate().toLocaleString(undefined, { maximumFractionDigits: 0 })
+    , 10, 20);
+}
+
+function addCircle(x, y, diameter, diameter2) {
+  stroke(0, 255, 0);
+  fill(0, 0, 255)
+  circle(x, y, diameter);
+
+  noFill();
+  circle(x, y, diameter2);
 }
 
 function onResize() {
@@ -36,4 +99,38 @@ function onResize() {
   parentHeight = canvasDiv.offsetHeight; // height of browser window
 
   resizeCanvas(parentWidth, parentHeight); // resize the window
+}
+
+function mouseClicked() {
+  for (let i = 0; i < active.length; i++) {
+    let clickData = active[i].click(mouseX, mouseY, song.currentTime());
+    if (clickData === -1) { // fail
+      break;
+    } else if (clickData === 1) { // success
+      break;
+    }
+  }
+}
+
+function keyPressed() {
+  console.log('Key Pressed: ' + keyCode);
+  if (keyCode === 90) { // z key
+
+  } else if (keyCode === 88) { // x key
+
+  } else if (keyCode === 27) { // escape key
+    if (isPlaying) { // pauses
+      frameRate(0);
+      song.pause();
+      cursor(ARROW);
+      isPlaying = false;
+    } else { // resumes
+      frameRate(120);
+      song.play();
+      noCursor();
+      isPlaying = true;
+    }
+  } else if (keyCode === 70) { // f key
+    fullscreen(!fullscreen());
+  }
 }
